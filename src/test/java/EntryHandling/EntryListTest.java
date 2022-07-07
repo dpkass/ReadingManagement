5790946c6f2967a2e9cac11d6bf84bbc0042d4c6
Taha El Amine Kassabi <taha.kassabi@hhu.de>
2022-07-08 01:24:38 +0200

    Add tests for EntryList

---
+++ src/test/java/EntryHandling/EntryListTest.java
@@ -0,0 +1,51 @@
+package EntryHandling;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName ("The EntryList should ")
+class EntryListTest {
+    EntryList el;
+
+    @BeforeEach
+    private void pre() {
+        el = new EntryList();
+    }
+
+    @Test
+    @DisplayName ("get Entries by Name")
+    void test_1() {
+        el.add(new String[] { "Solo Leveling" });
+        el.add(new String[] { "The Beginning after the End" });
+        el.add(new String[] { "Tales of Demons and Gods" });
+
+        assertThat(el.get("Solo Leveling")).isNotNull();
+        assertThat(el.get("The Beginning after the End")).isNotNull();
+        assertThat(el.get("Tales of Demons and Gods")).isNotNull();
+    }
+
+    @Test
+    @DisplayName ("get Entries by Acronym")
+    void test_2() {
+        el.add(new String[] { "Solo Leveling", "0", "null", "SL", "sl" });
+        el.add(new String[] { "The Beginning after the End", "0", "null", "TBATE", "tbate" });
+
+        assertThat(el.get("SL").name()).isEqualTo("Solo Leveling");
+        assertThat(el.get("sl").name()).isEqualTo("Solo Leveling");
+        assertThat(el.get("tbate").name()).isEqualTo("The Beginning after the End");
+    }
+
+    @Test
+    @DisplayName ("not find non existent Entries")
+    void test_3() {
+        el.add(new String[] { "Solo Leveling", "0", "null", "SL", "sl" });
+        el.add(new String[] { "The Beginning after the End", "0", "null", "TBATE", "tbate" });
+        el.add(new String[] { "Tales of Demons and Gods" });
+
+        assertThat(el.get("sth")).isNull();
+        assertThat(el.get("sth")).isNull();
+    }
+}
