a166462bbb0842b30a34448cba6c646418dc331f
Taha El Amine Kassabi <taha.kassabi@hhu.de>
2022-07-08 01:25:06 +0200

    Add mainclass

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,10 @@
+import Management.Manager;
+
 public class Main {
     public static void main(String[] args) {
+        Manager m = new Manager();
+        if (args.length == 1)
+            m.setFile(args[0]);
+        m.run();
     }
 }
