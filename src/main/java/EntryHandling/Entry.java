d33da0c6ba8990cdcac2ec15eb61ea82153c9396
Taha El Amine Kassabi <taha.kassabi@hhu.de>
2022-07-07 23:33:35 +0200

    Add plusRead method

--- src/main/java/EntryHandling/Entry.java
+++ src/main/java/EntryHandling/Entry.java
@@ -1,11 +1,15 @@
 package EntryHandling;
 
-public record Entry(String name, int chapter, String link) {
+public record Entry(String name, int readto, String link) {
     public Entry(String[] values) {
         this(values[0], Integer.parseInt(values[1]), values[2]);
     }
 
+    public Entry plusRead(int i) {
+        return new Entry(name, readto + i, link);
+    }
+
     public String toCSV() {
-        return name + ", " + chapter + ", " + link;
+        return name + ", " + readto + ", " + link;
     }
 }
