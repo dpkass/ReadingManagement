175df4e3d54433566c37b359f9c0ab1a36e84ac0
Taha El Amine Kassabi <taha.kassabi@hhu.de>
2022-07-07 23:32:24 +0200

    Add get (by name) method in EntryList

--- src/main/java/EntryHandling/EntryList.java
+++ src/main/java/EntryHandling/EntryList.java
@@ -6,12 +6,16 @@ import java.util.List;
 public class EntryList {
     public List<Entry> list = new ArrayList<>();
 
-    void add(String[] values) {
+    public void add(String[] values) {
         list.add(new Entry(values));
     }
 
-    void change(Entry oldEntry, Entry newEntry) {
+    public void change(Entry oldEntry, Entry newEntry) {
         if (list.remove(oldEntry))
             list.add(newEntry);
+    }
+
+    public Entry get(String s) {
+        return list.stream().filter(a -> a.name().equals(s)).findAny().orElse(null);
     }
 }
