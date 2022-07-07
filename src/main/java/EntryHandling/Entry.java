493113576a25e78984090e1f312c8286721aaf08
Taha El Amine Kassabi <taha.kassabi@hhu.de>
2022-07-07 23:47:02 +0200

    Change Entry to normal class

--- src/main/java/EntryHandling/Entry.java
+++ src/main/java/EntryHandling/Entry.java
@@ -1,12 +1,42 @@
 package EntryHandling;
 
-public record Entry(String name, int readto, String link) {
+public class Entry {
+    String name;
+    int readto;
+    String link;
+
     public Entry(String[] values) {
         this(values[0], Integer.parseInt(values[1]), values[2]);
     }
 
-    public Entry plusRead(int i) {
-        return new Entry(name, readto + i, link);
+    public Entry(String name, int readto, String link) {
+        this.name = name;
+        this.readto = readto;
+        this.link = link;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public int readto() {
+        return readto;
+    }
+
+    public String link() {
+        return link;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setReadto(int readto) {
+        this.readto = readto;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
     }
 
     public String toCSV() {
