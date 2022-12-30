5b2adbeaa4fb9623b91f44b20014049c5bd86702
Taha El Amine Kassabi <taha.kassabi@hhu.de>
2022-12-30 03:33:39 +0100

    Change file hierarchy -> new main -> upload as secondary site

--- src/main/java/AppRunner/Controllers/MainController.java
+++ src/main/java/AppRunner/Controllers/MainController.java
@@ -1,85 +1,13 @@
 package AppRunner.Controllers;
 
-import AppRunner.Datacontainers.FileNotValidException;
-import Management.Manager;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.multipart.MultipartFile;
-
-import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.IOException;
 
 @Controller
 public class MainController {
 
-    final FileService fs;
-    final WebApplicationContext ctx;
-
-    public MainController(FileService fs, WebApplicationContext ctx) {
-        this.fs = fs;
-        this.ctx = ctx;
-    }
-
     @GetMapping ("/")
     public String index() {
-        return "upload";
-    }
-
-    @GetMapping ("/ReadingManager-Files")
-    public String download(HttpServletResponse response) throws IOException {
-        RequestService rs = ctx.getBean(RequestService.class);
-        fs.makeZip(rs.files(), response.getOutputStream());
-        return "upload";
-    }
-
-    @PostMapping ("/")
-    public String fileupload(MultipartFile file, MultipartFile secretfile, Model m) {
-        Manager mgr = ctx.getBean(Manager.class);
-
-        handleFile(file, m, mgr);
-        handleSecretfile(secretfile, m, mgr);
-
-        return "upload";
-    }
-
-    private void handleFile(MultipartFile file, Model m, Manager mgr) {
-        if (file.isEmpty()) return;
-
-        File oldfile = mgr.file();
-        try {
-            File newfile = fs.store(file, false);
-            mgr.setFile(newfile);
-            mgr.loadFile();
-            m.addAttribute("fileupload", "File successfully uploaded.");
-            m.addAttribute("fileuploadtype", "valid-feedback");
-        } catch (FileNotValidException e) {
-            mgr.setFile(oldfile);
-            handleException(m, "fileupload");
-        }
-    }
-
-    private void handleSecretfile(MultipartFile secretfile, Model m, Manager mgr) {
-        if (secretfile.isEmpty()) return;
-
-        File oldsecretfile = mgr.secretfile();
-        try {
-            File newsecretfile = fs.store(secretfile, true);
-            mgr.setSecretfile(newsecretfile);
-            mgr.loadSecretFile();
-            m.addAttribute("secretfileupload", "File successfully uploaded.");
-            m.addAttribute("secretfileuploadtype", "valid-feedback");
-        } catch (FileNotValidException e) {
-            mgr.setSecretfile(oldsecretfile);
-            handleException(m, "secretfileupload");
-        }
-    }
-
-    private void handleException(Model m, String fileupload) {
-        m.addAttribute(fileupload, "File is not valid.");
-        m.addAttribute(fileupload + "type", "invalid-feedback");
+        return "index";
     }
 }
